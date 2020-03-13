3e6e85dcfced905009d7cc19ddd700e34c9a7a84
sen-viray <paula.viray5477@gmail.com>
2020-03-13 14:41:53 +0800

    Added Function

---
+++ functions-1.py
@@ -0,0 +1,25 @@
+hours = input("Enter Hours: ")
+try:
+    hours = int(hours)
+except:
+    hours = -1
+if hours < 0 :
+    print("Nice try, won't work though.")
+    pay = 0
+rate = input("Enter Rate: ")
+try:
+    rate = int(rate)
+except:
+    rate = -1
+if rate < 0 :
+    print("Already told you, that trick won't work. Try again.")
+    pay = 0
+else:
+    def computepay(hours, rate):
+        if int(hours) > 40 :
+            pay = int(hours) * (int (rate) * 1.5)
+            print("Pay: ", pay)
+        else :
+            pay = int(hours) * int(rate)
+            print("Pay: ", pay)
+    computepay(hours, rate)
