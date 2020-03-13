077a12d12b149543b3e6f7719b035b183f550041
sen-viray <paula.viray5477@gmail.com>
2020-03-13 13:45:00 +0800

    Added Try and Except

--- pay calculator.py
+++ pay calculator.py
@@ -1,5 +1,19 @@
 hours = input("Enter Hours: ")
+try:
+    hours = int(hours)
+except:
+    hours = -1
+if hours < 0:
+    print("Nice Try. Won't work though.")
+
 rate = input("Enter Rate: ")
+try:
+    rate = int(rate)
+except:
+    rate = -1
+if rate < 0:
+    print("Again, that isn't gonna work. Try again.")
+
 if int(hours) > 40 :
     pay = int(hours) * (int (rate) * 1.5)
 else :
