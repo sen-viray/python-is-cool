8c54b941514af201d93cd716f4f75daa5899ec5f
sen-viray <paula.viray5477@gmail.com>
2020-03-13 14:22:37 +0800

    Added conditionals

--- conditionals-exercise3.py
+++ conditionals-exercise3.py
@@ -1,11 +1,19 @@
 grade = input("Enter Grade:")
 try:
-    grade = str(grade)
+    grade = float(grade)
 except:
     grade = -1
-if str(grade) > 1.0:
-    print("Error")
-elif str(grade) >= 0.9:
+if grade < 0:
+        print("Not a grade.")
+elif float(grade) > 1.0:
+    print("Doesn't work like that unfortunately.")
+elif float(grade) >= 0.9:
     print("A")
-elif str(grade) >= 0.8:
+elif float(grade) >= 0.8:
     print("B")
+elif float(grade) >= 0.7:
+    print("c")
+elif float(grade) >= 0.6:
+    print("D")
+elif float(grade) < 0.6:
+    print("F")
